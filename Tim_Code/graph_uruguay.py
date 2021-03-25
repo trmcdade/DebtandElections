11a6b4eb9f257c6cdc43fe89ba6a3fcf79b21f28
trmcdade <45393549+trmcdade@users.noreply.github.com>
2021-03-25 12:13:10 -0400

    Update graph_uruguay.py

--- Tim_Code/graph_uruguay.py
+++ Tim_Code/graph_uruguay.py
@@ -10,31 +10,70 @@ fn = dir + "outstanding_regdata_usd_24m_2021-03-23.csv"
 df = pd.read_csv(fn, index_col=0)
 df = df[(df['Amt.Issued'] > 0)]
 df['Date'] = pd.to_datetime(df['Date'])
-# df = df[(df['system'] == 'Presidential') & (df['Amt.Mat'] > 0)]
+
+# check out the currencies in which countries have issued debt.
 countries = df['Country'].unique()
 [(i, df[df['Country'] == i]['Curr'].unique()) for i in countries]
-# df = df[(df['Year'] > 2001) & (df['Year'] < 2003)]
 
-# fig, ax = plt.subplots(figsize=(12, 6))
-gdf = df[df['Country'] == 'URUGUAY']
+cmcm_fn = dir + "cmcm_outstanding_regdata_usd_24m_2021-03-23.csv"
+df2 = pd.read_csv(cmcm_fn, index_col=0)
+df2 = df2[(df2['Amt.Numerator'] > 0)]
+df2['Date'] = pd.to_datetime(df2['Date'])
+
+cmcm2_fn = dir + "cmcm_outstanding_regdata_usd_120m_2021-03-23.csv"
+df3 = pd.read_csv(cmcm2_fn, index_col=0)
+df3 = df3[(df3['Amt.Numerator'] > 0)]
+df3['Date'] = pd.to_datetime(df3['Date'])
+
+## By date of issuance.
+gdf = df3[df3['Country'] == 'URUGUAY']
 fig = sns.jointplot(data = gdf,
-                    x='Date', y='Amt.Issued',
-                    # alpha=0.1,
+                    x = 'Date', y = 'Amt.Issued',
+                    alpha = 0.3,
                     hue = 'Curr',
+                    ylim = (0, gdf['Amt.Issued'].max()),
                     xlim = (gdf['Date'].min(), gdf['Date'].max())
-                    # ylim = (-0.5, 0.5)
                     )
 plt.subplots_adjust(top=0.9)
 fig.set_axis_labels('Time', 'Amount of Debt Issued')
-# g.fig.suptitle(f'Protest Count Over Time by Country')
+# g.fig.suptitle(f'')
 plt.tight_layout()
-uruguay = dir + f'uruguay_by_curr.png'
+uruguay = dir + f'uruguay_curr_time.png'
 plt.savefig(uruguay)
 
-uruguay
+## By maturity length.
 
-# x_dates = df['Date'].dt.strftime('%Y-%m-%d').sort_values().unique()
-# ax.set_xticklabels(labels=x_dates, rotation=45, ha='right')
+udf = df3[df3['Country'] == 'URUGUAY']
+fig = sns.jointplot(data = udf,
+                    x = 'Date', y = 'Mty',
+                    alpha = 0.3,
+                    hue = 'Curr',
+                    xlim = (udf['Date'].min(), udf['Date'].max()),
+                    ylim = (-2, udf['Mty'].max() + 1)
+                    )
+plt.subplots_adjust(top=0.9)
+fig.set_axis_labels('Time', 'Maturity (Months)')
+# g.fig.suptitle(f'')
+plt.tight_layout()
+uruguay = dir + f'uruguay_mty_time.png'
+plt.savefig(uruguay)
+
+## By maturity length.
+
+mdf = df3[df3['Country'] == 'URUGUAY']
+fig = sns.jointplot(data = mdf,
+                    x = 'Mty', y = 'Amt.Numerator',
+                    alpha = 0.2,
+                    hue = 'Curr',
+                    xlim = (mdf['Mty'].min() - 1, mdf['Mty'].max() + 1),
+                    ylim = (0, mdf['Amt.Numerator'].max())
+                    )
+plt.subplots_adjust(top=0.9)
+fig.set_axis_labels('Maturity (Months)', 'Amount of Debt Issued')
+# g.fig.suptitle(f'')
+plt.tight_layout()
+uruguay = dir + f'uruguay_mty_curr.png'
+plt.savefig(uruguay)
 
 mu = np.mean(df['DV'])
 std = np.std(df['DV'])
